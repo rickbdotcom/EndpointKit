8ceb0ceba23e09cf1a7259c45abbf20355746f8c
Richard Burgess <rickb@rickb.com>
2023-09-06 12:50:03 -0500

    any api endpoint

---
+++ Sources/EndpointKit/APIEndpoint/AnyAPIEndpoint.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by Burgess, Rick (CHICO-C) on 9/6/23.
+//
+
+import Foundation
+
+public struct AnyAPIEndpoint<T: APIEndpoint>: APIEndpoint {
+    public typealias Parameters = T.Parameters
+    public typealias Response = T.Response
+
+    public var parameters: T.Parameters
+    public var endpoint: Endpoint
+
+    public var parameterEncoder: any ParameterEncoder<T.Parameters>
+    public var responseDecoder: any ResponseDecoder<T.Response>
+
+    public init(
+        parameters: Parameters,
+        endpoint: Endpoint,
+        parameterEncoder: any ParameterEncoder<Parameters>,
+        responseDecoder: any ResponseDecoder<Response>
+    ) {
+        self.parameters = parameters
+        self.endpoint = endpoint
+        self.parameterEncoder = parameterEncoder
+        self.responseDecoder = responseDecoder
+    }
+
+    public init<Endpoint: APIEndpoint>(_ endpoint: Endpoint)
+        where Endpoint.Parameters == Parameters, Endpoint.Response == Response {
+        self.parameters = endpoint.parameters
+        self.endpoint = endpoint.endpoint
+        self.parameterEncoder = endpoint.parameterEncoder
+        self.responseDecoder = endpoint.responseDecoder
+    }
+}
+
+public extension APIEndpoint {
+
+    func any() -> AnyAPIEndpoint<Self> {
+        AnyAPIEndpoint(self)
+    }
+}
