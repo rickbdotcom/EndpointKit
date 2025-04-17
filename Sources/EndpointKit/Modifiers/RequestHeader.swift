22c668ded1c48e2d7b93ccf6e8dfb450f9db50f2
Richard Burgess <rickb@rickb.com>
2025-04-17 12:12:18 -0500

    merge

--- Sources/EndpointKit/Modifiers/RequestHeader.swift
+++ Sources/EndpointKit/Modifiers/RequestHeader.swift
@@ -29,23 +29,40 @@ extension RequestEncoder {
         uniquingKeysWith combine: @escaping (String, String) -> String = { a, _ in a }
     ) -> any RequestEncoder<Parameters> {
         AnyRequestEncoder { parameters, request in
-            var request = try await encode(parameters, into: request)
-            let requestHeaders = request.allHTTPHeaderFields ?? [:]
-            request.allHTTPHeaderFields = headers.merging(requestHeaders, uniquingKeysWith: combine)
-            return request
+            try await encode(parameters, into: request).merge(
+                headers: headers,
+                uniquingKeysWith: combine
+            )
         }
     }
 
     /// Modify parameter encoder to remove headers
     public func remove(headers: [String]) -> any RequestEncoder<Parameters> {
         AnyRequestEncoder { parameters, request in
-            var request = try await encode(parameters, into: request)
-            if let requestHeaders = request.allHTTPHeaderFields {
-                request.allHTTPHeaderFields = requestHeaders.filter { key, _ in
-                    headers.contains(key) == false
-                }
-            }
-            return request
+            try await encode(parameters, into: request).remove(headers: headers)
         }
+    }
+}
+
+extension URLRequest {
+    public func merge(
+        headers: [String: String],
+        uniquingKeysWith combine: @escaping (String, String) -> String = { a, _ in a }
+    ) -> URLRequest {
+        var request = self
+        let requestHeaders = request.allHTTPHeaderFields ?? [:]
+        request.allHTTPHeaderFields = headers.merging(requestHeaders, uniquingKeysWith: combine)
+        return request
+    }
+
+
+    public func remove(headers: [String]) -> URLRequest {
+        var request = self
+        if let requestHeaders = request.allHTTPHeaderFields {
+            request.allHTTPHeaderFields = requestHeaders.filter { key, _ in
+                headers.contains(key) == false
+            }
+        }
+        return request
     }
 }
