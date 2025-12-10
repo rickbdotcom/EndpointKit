3ff8c547a9e36f74578abe2b90a7ab1c200feea6
Richard Burgess <richard.burgess@wexinc.com>
2025-12-10 09:58:13 -0600

    URLRequestModifier

---
+++ Sources/EndpointKit/Modifiers/RequestURLRequest.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  EndpointKit
+//
+//  Created by Richard Burgess on 12/10/25.
+//
+
+import Foundation
+
+public protocol URLRequestModifier {
+    func modify(_ request: URLRequest) -> URLRequest 
+}
+
+public extension AnyEndpointModifier {
+    
+    static func modify(_ modifier: any URLRequestModifier) -> AnyEndpointModifier {
+        RequestModifier { encoder, parameters, request in
+            try await modifier.modify(encoder.encode(parameters, into: request))
+        }.any()
+    }
+}
