c8c7a4cd53643b95b4bf6ba891b949e1f1cdf466
Richard Burgess <richard.burgess@wexinc.com>
2026-01-05 15:34:06 -0600

    URLRequestModifier

---
+++ Sources/EndpointKit/Modifiers/EndpointModifiers.swift
@@ -0,0 +1,27 @@
+//
+//  EndpointModifiers.swift
+//  backoff
+//
+//  Created by Richard Burgess on 1/2/26.
+//
+
+import Foundation
+
+public protocol EndpointModifiers {
+    func modifiers<T: EndpointKit.Endpoint>(for endpoint: T) -> [AnyEndpointModifier<T.Parameters, T.Response>] 
+}
+
+public extension EndpointModifiers where Self == EndpointModifiersArray {
+    
+    static func array(_ array: [any EndpointModifiers]) -> Self {
+        .init(array: array)
+    }
+}
+
+public struct EndpointModifiersArray: EndpointModifiers {
+    let array: [EndpointModifiers]
+
+    public func modifiers<T: EndpointKit.Endpoint>(for endpoint: T) -> [AnyEndpointModifier<T.Parameters, T.Response>] {
+        array.flatMap { $0.modifiers(for: endpoint) }
+    }
+}
---
+++ Sources/EndpointKit/Modifiers/URLRequestModifier.swift
@@ -0,0 +1,53 @@
+//
+//  URLRequestModifier.swift
+//  backoff
+//
+//  Created by Richard Burgess on 1/2/26.
+//
+
+import Foundation
+
+public protocol URLRequestModifier {
+    func callAsFunction(_ urlRequest: URLRequest) async throws -> URLRequest
+}
+
+public struct AnyURLRequestModifier: URLRequestModifier {
+    let modify: (URLRequest) async throws -> URLRequest
+    
+    public init(_ modify: @escaping (URLRequest) async throws -> URLRequest) {
+        self.modify = modify
+    }
+    
+    public func callAsFunction(_ urlRequest: URLRequest) async throws -> URLRequest {
+        try await modify(urlRequest)
+    }
+}
+
+public struct URLRequestModifiers: EndpointModifiers {
+    let requestModifiers: [URLRequestModifier]
+    
+    public init(_ requestModifiers: [URLRequestModifier]) {
+        self.requestModifiers = requestModifiers
+    }
+    
+    public func modifiers<T: EndpointKit.Endpoint>(for endpoint: T) -> [AnyEndpointModifier<T.Parameters, T.Response>] {
+        requestModifiers.map { $0.asRequestModifier().any() }
+    }
+}
+
+public extension URLRequestModifier {
+    
+    func asRequestModifier<Parameters, Response>() -> RequestModifier<Parameters, Response> {
+        RequestModifier(self)
+    }
+}
+
+public extension RequestModifier {
+    
+    init(_ modifier: URLRequestModifier) {
+        self.init { encoder, parameters, request in
+            try await modifier(encoder.encode(parameters, into: request))
+        }
+    }
+}
+
