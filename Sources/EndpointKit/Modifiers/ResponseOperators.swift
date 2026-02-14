d5f6a22bd1c1ab381b2fe05f7e2d5407597d05ba
Richard Burgess <rick@Janos-MacBook-Pro.local>
2026-02-14 00:29:49 -0600

    additional modifiers

--- Sources/EndpointKit/Modifiers/ResponseOperators.swift
+++ Sources/EndpointKit/Modifiers/ResponseOperators.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+extension AnyEndpointModifier {
+    /// Create a modifier that modifies the endpoint's Content-Type
+    public static func map(
+        _ replace: @Sendable @escaping (Response) async throws -> Response
+    ) -> Self {
+        ResponseModifier {
+            $0.map(replace)
+        }.any()
+    }
+
+    public func replaceError(
+        _ replace: @Sendable @escaping (Error) async throws -> Response
+    ) -> Self {
+        ResponseModifier {
+            $0.replaceError(replace)
+        }.any()
+    }
+}
+
 extension ResponseDecoder {
 
     public func map (
